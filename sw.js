d1e4d560fe0c05437803452895b6e3a4ad549ee7
悠哉 <212059853+fuchenyouzai@users.noreply.github.com>
2025-09-20 13:43:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'youzai-blog-cache-v1';
+const CACHE_NAME = 'youzai-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,16 +7,30 @@ const urlsToCache = [
   './icon-512.png'
 ];
 
+// 安装 SW 并缓存
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// 激活 SW
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// 请求拦截
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
